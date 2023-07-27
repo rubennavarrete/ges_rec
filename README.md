cec8145aece853a73cf5e741ff0b8991642a831f
Ruben valencia <rd_navarrete@outlook.com>
2023-07-27 16:22:56 -0500

    update(package):configuracion inicial para subir a produccion

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ges_rec
+
 Desarrollo de aplicativo web para la gestion de recetas médicas
+
+En este repositorio se esta guarda tanto el Backend como el Frontend - MONOREPO
+
+- Backend
+
+```bash
+cd client
+npm install
+npm run dev // yarn dev
+```
+
+- Frontend
+
+```bash
+cd server
+npm install
+npm run dev // yarn dev
+```
--- server/src/database/database.js
+++ server/src/database/database.js
@@ -1,6 +1,19 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("ges_rec","postgres","12345",{
-    host: "localhost",
+// Conexión a la base de datos local
+// export const sequelize = new Sequelize("ges_rec","postgres","12345",{
+//     host: "localhost",
+//     dialect: "postgres",
+// })
+
+// Conexión a la base de datos en la nube
+export const sequelize = new Sequelize(
+  "railway",
+  "postgres",
+  "r8PaWGOjkaGmEsu87VVL",
+  {
+    host: "containers-us-west-57.railway.app",
     dialect: "postgres",
-})
+    port: 5469,
+  }
+);
