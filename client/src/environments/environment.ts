e718c0e8f48f4ec6089873478142c07d64b53397
Aresx12 <henryrayos69@gmail.com>
2023-11-05 17:01:26 -0500

    EMPEZAAR TESIS LOCAL

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,15 +1,19 @@
+// //enviromen local
 const baseUrl = '';
 export const environment = {
   production: false,
   baseUrl: baseUrl,
-  url: 'https://localhost:4200/' + baseUrl,
+    url: 'https://localhost:4200/' + baseUrl,
   urlLogOut: 'https://localhost:4200/' + baseUrl,
-  // urlOneDriveService:
-  //   'https://pruebas.espoch.edu.ec:8181/WebCorreoInstitucional/ServiciosCorreos/TokenOneDrive',
-
-   //urlApi: 'http://26.63.14.26:4000/',    
-   urlApi: 'http://localhost:4000/',               //Local 
- 
-
-  // CodigoSistemaOneDrive: 'ARCHPOLI',
+  urlApi: 'http://localhost:4000/',
 };
+
+/* /* enviromen produccion
+const baseUrl = '';
+export const environment = {
+  production: false,
+  baseUrl: baseUrl,
+  url: 'https://ges-rec.vercel.app/' + baseUrl,
+  urlLogOut: 'https://ges-rec.vercel.app/' + baseUrl,
+  urlApi: 'https://gesrec-production.up.railway.app/',
+}; */
